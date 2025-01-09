3ba73699ae3504af5c0f0e451f03f5c74d67e1dd
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2025-01-09 17:03:38 -0300

    feat: create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { BackgroundImage } from "../styles/loginScreen.styles";
+
+const LoginScreen: React.FC = () => {
+
+    return (
+        <div>
+            <BackgroundImage src="./background.png" />
+        </div>
+    );
+};
+
+export default LoginScreen;
