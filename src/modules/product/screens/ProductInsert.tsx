e7a843a48d9710703ffe8c20100c115256b236d6
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2025-01-14 11:40:04 -0300

    feat: refactor insert Product

--- src/modules/product/screens/ProductInsert.tsx
+++ src/modules/product/screens/ProductInsert.tsx
@@ -5,12 +5,16 @@ import { useDataContext } from "../../../shared/hooks/useDataContext"
 import { useRequests } from "../../../shared/hooks/useRequests"
 import { MethodsEnum } from "../../../shared/enums/methods.enum"
 import { URL_CATEGORY, URL_PRODUCT } from "../../../shared/constants/urls"
-import { LimitedContainer } from "../styles/productInsert.style"
+import { ProductInsertContainer } from "../styles/productInsert.style"
 import Input from "../../../shared/components/inputs/input/input"
 import Button from "../../../shared/components/buttons/button/button"
 import Select from "../../../shared/components/inputs/select/select"
 import { InsertProductDto } from "../../../shared/dtos/InsertProduct.dto"
 import { connectionAPIPost } from "../../../shared/functions/connection/connectionAPI"
+import { LimitedContainer } from "../../../shared/components/styles/limited.styled"
+import { DisplayFlexJustifyRight } from "../../../shared/components/styles/display.styled"
+import { useNavigate } from "react-router-dom"
+import { useGlobalContext } from "../../../shared/hooks/useGlobalContext"
 
 const ProductInsert = () => {
 
@@ -20,7 +24,9 @@ const ProductInsert = () => {
 		image: ''
 	})
 	const { categories, setCategories } = useDataContext()
+	const { setNotification } = useGlobalContext()
 	const { request } = useRequests()
+	const navigate = useNavigate()
 
 	useEffect(() => {
 		if (categories.length === 0) {
@@ -35,22 +41,22 @@ const ProductInsert = () => {
 		})
 	}
 
-	const onChange = (e: React.ChangeEvent<HTMLInputElement>, nameObject: string) => {
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>, nameObject: string, isNumber: boolean) => {
 		setProduct({
 			...product,
-			[nameObject]: e.target.value
+			[nameObject]: isNumber ? Number(e.target.value) : e.target.value
 		})
 	}
 
-	const onChangePrice = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setProduct({
-			...product,
-			price: Number(e.target.value)
-		})
+	const handleInsertProduct = async () => {
+		await connectionAPIPost(URL_PRODUCT, product).then(() => {
+			setNotification("Sucesso!", 'success', 'Produto inserido com sucesso.')
+			navigate(ProductRoutesEnum.PRODUCT)
+		}).catch((e: Error) => console.log(setNotification(e.message, 'error')))
 	}
 
-	const handleInsertProduct = () => {
-		connectionAPIPost(URL_PRODUCT, product)
+	const handleOnClickCancel = () => {
+		navigate(ProductRoutesEnum.PRODUCT)
 	}
 
 	return (
@@ -66,24 +72,33 @@ const ProductInsert = () => {
 				name: 'INSERIR PRODUTO',
 			},
 		]}>
-			<LimitedContainer>
-				<Input onChange={(e) => onChange(e, 'name')} value={product.name} margin="0px 0px 16px 0" title="Nome" placeholder="Nome" />
-				<Input onChange={(e) => onChange(e, 'image')} value={product.image} margin="0px 0px 16px 0" title="Url imagem" placeholder="Url imagem" />
-				<Input onChange={onChangePrice} value={product.price} margin="0px 0px 16px 0" title="Preço" placeholder="Preço" />
-				<Select
-					title="Categoria"
-					margin="0px 0px 32px 0"
-					onChange={handleChange}
-					options={
-						categories.map((category) => ({
-							value: `${category.id}`,
-							label: `${category.name}`,
-						}))
-					}
-				/>
-				<Button onClick={handleInsertProduct} type="primary">Inserir Produto</Button>
-			</LimitedContainer>
-		</Screen>
+			<ProductInsertContainer>
+				<LimitedContainer width={400}>
+					<Input onChange={(e) => onChange(e, 'name', false)} value={product.name} margin="0px 0px 16px 0" title="Nome" placeholder="Nome" />
+					<Input onChange={(e) => onChange(e, 'image', false)} value={product.image} margin="0px 0px 16px 0" title="Url imagem" placeholder="Url imagem" />
+					<Input onChange={(e) => onChange(e, 'price', true)} value={product.price} margin="0px 0px 16px 0" title="Preço" placeholder="Preço" />
+					<Select
+						title="Categoria"
+						margin="0px 0px 32px 0"
+						onChange={handleChange}
+						options={
+							categories.map((category) => ({
+								value: `${category.id}`,
+								label: `${category.name}`,
+							}))
+						}
+					/>
+					<DisplayFlexJustifyRight>
+						<LimitedContainer margin="0px 8px" width={120}>
+							<Button danger onClick={handleOnClickCancel}>Cancelar</Button>
+						</LimitedContainer>
+						<LimitedContainer width={120}>
+							<Button onClick={handleInsertProduct} type="primary">Inserir Produto</Button>
+						</LimitedContainer>
+					</DisplayFlexJustifyRight>
+				</LimitedContainer>
+			</ProductInsertContainer>
+		</Screen >
 	)
 }
 
