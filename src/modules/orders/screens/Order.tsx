91a28e4bc052eb6b80f673d65bbe60fdf283619d
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2025-01-16 10:31:46 -0300

    feat: insert table orders

--- src/modules/orders/screens/Order.tsx
+++ src/modules/orders/screens/Order.tsx
@@ -1,16 +1,53 @@
+import { ColumnsType } from "antd/es/table"
 import Screen from "../../../shared/components/screen/Screen"
 import { useOrder } from "../hooks/useOrder"
+import { OrderType } from "../../../shared/types/OrderType"
+import Table from "../../../shared/components/table/Table"
+
+const columns: ColumnsType<OrderType> = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+        render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'Data',
+        dataIndex: 'date',
+        key: 'date',
+        render: (text) => <a>{text}</a>
+    },
+    {
+        title: 'Usuário',
+        dataIndex: 'user',
+        key: 'user',
+        sorter: (a, b) => a.user.name.localeCompare(b.user.name),
+        render: (_, target) => <a>{target.user?.name}</a>
+    },
+    {
+        title: 'Produtos',
+        dataIndex: 'amountProducts',
+        key: 'amountProducts',
+        render: (text) => <a>{text}</a>,
+    },
+
+]
+
+const listBreadcrumb = [
+    {
+        name: 'HOME',
+    },
+    {
+        name: 'PEDIDOS',
+    },
+]
 
 const Order = () => {
 
     const { orders } = useOrder()
-
-    console.log(orders, 'orders');
-
-
     return (
-        <Screen>
-            <>teste</>
+        <Screen listBreadcrumb={listBreadcrumb}>
+            <Table columns={columns} dataSource={orders} />
         </Screen>
     )
 }
